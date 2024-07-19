a571a17a8bf8a0ce945f94d28cfe145f79c4c4f4
staticGuru <vigneshguru274@gmail.com>
2024-07-19 19:20:32 +0530

    fix: update the ts type fields in questions

---
+++ backend/src/infra/http/controllers/quiz-controller.ts
@@ -0,0 +1,33 @@
+import { GetQuizUseCase } from '@backend/domain/quiz/use-cases/get-quiz-use-cases';
+import { SubmitQuizUseCase } from '@backend/domain/quiz/use-cases/submit-quiz-use-cases';
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+
+@Controller('/api/quiz')
+@ApiTags('quiz')
+export class QuizController {
+  constructor(
+    private readonly getQuiz: GetQuizUseCase,
+    private readonly submitQuiz: SubmitQuizUseCase,
+  ) {}
+
+  @Get(':id')
+  async getQuizById(@Param('id') id: number) {
+    const quiz = await this.getQuiz.execute(id);
+    if (!quiz) {
+      return { message: 'Quiz not found' };
+    }
+    return quiz;
+  }
+
+  @Post(':id/submit')
+  async submitQuizFunction(
+    @Param('id') quizId: number,
+    @Body('userId') userId: number,
+    @Body('progress') progress: string,
+  ) {
+    const score = await this.submitQuiz.execute(quizId, userId, progress);
+
+    return { score };
+  }
+}
--- frontend/src/common/components/quiz/types.ts
+++ frontend/src/common/components/quiz/types.ts
@@ -5,9 +5,20 @@ export type AnswerStates = (typeof ANSWER_STATES)[keyof typeof ANSWER_STATES];
 export type Difficulty = 'easy' | 'regular' | 'hard' | 'difficult';
 
 export type Question = {
+  id: number;
   question: string;
   answers: string[];
   correctAnswer: number;
   correctAnswerDetail: string;
   difficulty: Difficulty;
+};
+
+export type SubmitQuizPayload = {
+  quizId: number;
+  userId: number;
+  progress: string;
+};
+
+export type SubmitQuizResponse = {
+  score: number;
 };

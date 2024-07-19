4952a6db1ddf80951ca0cddc4bb418f222dd5b76
staticGuru <vigneshguru274@gmail.com>
2024-07-19 19:13:19 +0530

    feat: integrate the query and mutation callbacks in quizview

--- frontend/src/domain/quiz/components/quiz-view/index.tsx
+++ frontend/src/domain/quiz/components/quiz-view/index.tsx
@@ -5,7 +5,7 @@ import { useRouter } from 'next/navigation';
 import { Question } from '@frontend/common/components/quiz/types';
 import { QuizView as QuizViewCommon } from '@frontend/common/components/quiz';
 
-import { McqQuestions } from './data';
+import { useFetchQuizById, useSubmitQuizMutation } from '../../hooks';
 
 type QuizViewProps = {
   id: number;
@@ -13,18 +13,32 @@ type QuizViewProps = {
 
 export const QuizView = ({ id }: QuizViewProps) => {
   const router = useRouter();
+  const { data, isLoading } = useFetchQuizById(id);
+  const { mutate } = useSubmitQuizMutation();
 
-  const questions: Question[] = McqQuestions;
-
-  const onQuizCompletion = () => {
-    router.push(`/account/quiz/quiz_completed?score=30`);
+  const onQuizCompletion = ({ quizProgress }) => {
+    mutate(
+      {
+        progress: JSON.stringify(quizProgress),
+        quizId: id,
+        userId: 67373, //TODO: change it in dynamic user id setup
+      },
+      {
+        onSuccess: ({ score }) => {
+          router.push(`/account/quiz/quiz_completed?score=${score}`);
+        },
+        onError: (error) => {
+          console.error('Error submitting quiz:', error);
+        },
+      },
+    );
   };
 
   return (
     <QuizViewCommon
       headerType="progress"
-      questions={questions}
-      isLoading={false}
+      questions={data}
+      isLoading={isLoading}
       error={null}
       onRetry={() => {}}
       onSubmit={onQuizCompletion}

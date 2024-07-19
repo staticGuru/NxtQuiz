f1466c1bb763ccbed3beabdeaf0e51cecc628a39
staticGuru <vigneshguru274@gmail.com>
2024-07-19 19:11:20 +0530

    feat: add react queries for fetch and mutate the items

---
+++ frontend/src/domain/quiz/hooks/index.ts
@@ -0,0 +1,39 @@
+import { ApiService } from '@frontend/lib/apiService';
+import {
+  useMutation,
+  UseMutationResult,
+  useQuery,
+} from '@tanstack/react-query';
+import {
+  SubmitQuizPayload,
+  SubmitQuizResponse,
+} from '@frontend/common/components/quiz/types';
+
+import { mapQuizQuestions } from '../mappers';
+
+export const useFetchQuizById = (quizId: number) => {
+  return useQuery<any, Error>({
+    queryKey: ['quiz', quizId],
+    queryFn: async (): Promise<any> => {
+      const data = await ApiService.get(`/quiz/${quizId}`);
+      return mapQuizQuestions(data);
+    },
+    enabled: !!quizId,
+  });
+};
+
+export const useSubmitQuizMutation = (): UseMutationResult<
+  SubmitQuizResponse,
+  Error,
+  SubmitQuizPayload
+> => {
+  return useMutation<SubmitQuizResponse, Error, SubmitQuizPayload>({
+    mutationFn: async (payload: SubmitQuizPayload) => {
+      const response = await ApiService.post(
+        `/quiz/${payload.quizId}/submit`,
+        payload,
+      );
+      return response;
+    },
+  });
+};

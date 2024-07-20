ab89f8517acf2d4e9bf922b1e4420a8d06cfab8a
staticGuru <vigneshguru274@gmail.com>
2024-07-20 20:14:02 +0530

    fix: remove the question repository class

--- backend/src/domain/quiz/repositories/quiz-repository.ts
+++ backend/src/domain/quiz/repositories/quiz-repository.ts
@@ -1,9 +1,5 @@
-import { Question, Quiz } from '../entities/quiz-entity';
+import { Quiz } from '../entities/quiz-entity';
 
 export abstract class QuizRepository {
   abstract findById(id: number): Promise<Quiz | null>;
-}
-
-export abstract class QuestionRepository {
-  abstract findByQuizId(quizId: number): Promise<Question[]>;
 }
